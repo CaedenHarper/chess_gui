c887e13b0ffc4201a744b5ef5525e4a5d3e27d27
Caeden Harper <144764803+CaedenHarper@users.noreply.github.com>
2026-05-25 18:43:25 -0700

    Improve menu (#8)
    * pass RenderState by reference to prevent unnecessary copies
    * remove old pieceSprite code
    * improve menu screen look

--- src/gui/main_menu/MainMenuRenderer.cpp
+++ src/gui/main_menu/MainMenuRenderer.cpp
@@ -7,44 +7,92 @@
 void MainMenuRenderer::render() {
     clearWindow(sf::Color::Black);
 
+    drawBackground();
+    drawButton(MainMenuLayout::menuCard);
     drawTitle();
-    drawWhiteButton();
-    drawBlackButton();
+    drawSubtitle();
+    drawButton(MainMenuLayout::whiteButton);
+    drawButton(MainMenuLayout::blackButton);
+    drawFooter();
 
     window_->display();
 }
 
+void MainMenuRenderer::drawBackground() {
+    constexpr float squareSize = 125.F;
+
+    for(int row = 0; row < 8; ++row) {
+        for(int col = 0; col < 8; ++col) {
+            sf::RectangleShape square{{squareSize, squareSize}};
+            square.setPosition({col * squareSize, row * squareSize});
+
+            const bool light = (row + col) % 2 == 0;
+            square.setFillColor(light ? MainMenuLayout::lightBackgroundColor : MainMenuLayout::darkBackgroundColor);
+
+            window_->draw(square);
+        }
+    }
+}
+
 void MainMenuRenderer::drawTitle() {
-    constexpr int titleFontSize = 96;
-    constexpr sf::Vector2f titlePosition{350.F, 120.F};
+    constexpr sf::Vector2f titlePosition{500.F, 220.F};
+    constexpr int titleSize = 96;
 
-    drawText("Chess", titlePosition, titleFontSize);
+    // Shadow
+    drawText("CHESS", {titlePosition.x + 4.F, titlePosition.y + 4.F}, titleSize, sf::Color{20, 20, 20}, true);
+
+    // Main title
+    drawText("CHESS", titlePosition, titleSize, sf::Color{245, 245, 245}, true);
+
+    // Gold underline
+    constexpr sf::Vector2f underlineSize{180.F, 3.F};
+    constexpr sf::Vector2f underlinePosition{titlePosition.x - underlineSize.x / 2.F, titlePosition.y + 40.F};
+
+    sf::RectangleShape underline{underlineSize};
+    underline.setPosition(underlinePosition);
+    underline.setFillColor(sf::Color{212, 175, 55});
+
+    window_->draw(underline);
 }
 
-void MainMenuRenderer::drawWhiteButton() {
-    drawButton(MainMenuLayout::whiteButton);
+void MainMenuRenderer::drawSubtitle() {
+    drawText("Choose your side", {500.F, 300.F}, 28, sf::Color{180, 180, 180}, true);
 }
 
-void MainMenuRenderer::drawBlackButton() {
-    drawButton(MainMenuLayout::blackButton);
+void MainMenuRenderer::drawFooter() {
+    drawText("SFML Chess Engine", {500.F, 625.F}, 20, sf::Color{120, 120, 120}, true);
 }
 
 void MainMenuRenderer::clearWindow(const sf::Color& backgroundColor) {
     window_->clear(backgroundColor);
 }
 
-void MainMenuRenderer::drawText(const std::string& str, const sf::Vector2f& position, int size) {
+void MainMenuRenderer::drawText(
+    const std::string& str,
+    const sf::Vector2f& position,
+    int size,
+    const sf::Color& color,
+    bool centered
+) {
     sf::Text text{RenderUtils::FONT};
 
     text.setString(str);
-    text.setPosition(position);
-    text.setFillColor(sf::Color::White);
     text.setCharacterSize(size);
+    text.setFillColor(color);
+
+    if(centered) {
+        const sf::FloatRect bounds = text.getLocalBounds();
+        text.setOrigin({bounds.position.x + bounds.size.x / 2.F, bounds.position.y + bounds.size.y / 2.F});
+    }
+
+    text.setPosition(position);
 
     window_->draw(text);
 }
 
-void MainMenuRenderer::drawButton(MainMenuButton button) {
+void MainMenuRenderer::drawButton(const MainMenuButton& button) {
     window_->draw(button.button());
-    window_->draw(button.text());
+    if(button.hasText()) {
+        window_->draw(*button.text());
+    }
 }
